f413dd54d9eac5c674a27335044bb7efb45eb62f
lu123xin <150250713+lu123xin@users.noreply.github.com>
2025-08-09 19:24:15 +0800

    train.py

--- code/train.py
+++ code/train.py
@@ -12,18 +12,25 @@ import torch
 import torch.optim as optim
 from torchvision import transforms
 import torch.nn.functional as F
-import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
 
 from utils import ramps, losses, metrics, test_patch
-from utils.losses import  dice_loss
+from utils.losses import dice_loss
 from dataloaders.dataset import *
 from networks.net_factory import net_factory
 
 
 def get_current_consistency_weight(epoch):
     return args.consistency * ramps.sigmoid_rampup(epoch, args.consistency_rampup)
+
+
+def augment(volume):
+    if random.random() > 0.5:
+        volume = torch.flip(volume, dims=[3])  
+    if random.random() > 0.5:
+        volume = torch.rot90(volume, k=1, dims=[3, 4])  # H-D 
+    return volume
 
 
 parser = argparse.ArgumentParser()
@@ -67,7 +74,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 labeled_bs = args.labeled_bs
 max_iterations = args.max_iteration
 base_lr = args.base_lr
-var_param_conflict_weight=0.5
+
 if args.deterministic:
     cudnn.benchmark = False
     cudnn.deterministic = True
@@ -77,7 +84,7 @@ if args.deterministic:
     np.random.seed(args.seed)
 
 if __name__ == "__main__":
-    ## make logger file
+    # 创建保存路径及代码备份
     if not os.path.exists(snapshot_path):
         os.makedirs(snapshot_path)
     if os.path.exists(snapshot_path + '/code'):
@@ -93,6 +100,8 @@ if __name__ == "__main__":
     logging.info(sys.argv[0])
 
     model = net_factory(net_type=args.model, in_chns=1, class_num=num_classes, mode="train")
+    model = model.cuda()
+
     if args.dataset_name == "LA":
         db_train = LAHeart(base_dir=train_data_path,
                            split='train',
@@ -108,6 +117,7 @@ if __name__ == "__main__":
                                 RandomCrop(patch_size),
                                 ToTensor(),
                             ]))
+
     labelnum = args.labelnum
     labeled_idxs = list(range(labelnum))
     unlabeled_idxs = list(range(labelnum, args.max_samples))
@@ -128,174 +138,66 @@ if __name__ == "__main__":
     optimizer = optim.SGD(model.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0001)
 
     writer = SummaryWriter(snapshot_path + '/log')
-    logging.info("{} itertations per epoch".format(len(trainloader)))
-    pixel_criterion = losses.ce_loss_mask
-    consistency_criterion = nn.CrossEntropyLoss(reduction='none')
-    dice_loss = losses.Binary_dice_loss
+    logging.info("{} iterations per epoch".format(len(trainloader)))
 
     iter_num = 0
     best_dice = 0
     max_epoch = max_iterations // len(trainloader) + 1
-    lr_ = base_lr
     iterator = tqdm(range(max_epoch), ncols=70)
 
     for epoch_num in iterator:
         for i_batch, sampled_batch in enumerate(trainloader):
-
             volume_batch, label_batch = sampled_batch['image'], sampled_batch['label']
             volume_batch, label_batch = volume_batch.cuda(), label_batch.cuda()
 
-
             model.train()
-            for num in range(3):
-                if num == 0:
-                    outputs, outfeats,mask,stage_out1= model(volume_batch, [])
-                else:
-                    outputs, outfeats ,mask,stage_out1= model(volume_batch, en)
 
-                consistency_weight = get_current_consistency_weight(iter_num // 150)
+         
+            volume_batch_aug = augment(volume_batch)
 
-                en = []
-                for idx in range(len( mask[0])):
-                    mask1 =  mask[0][idx].detach()
-                    mask2 =  mask[1][idx].detach()
-                    # mask1 = sharpening(mask1)
-                    # mask2 = sharpening(mask2)
-                    en.append(1e-3 * (mask1 - mask2))
-                print('en',en[3].shape)
+            
+            model.eval()
+            with torch.no_grad():
+                preds_orig = model(volume_batch)  # [B, C, W, H, D]
+                preds_aug = model(volume_batch_aug)
+            model.train()
 
-                num_outputs = len(outputs)
+            preds_orig_prob = F.softmax(preds_orig, dim=1)
+            preds_aug_prob = F.softmax(preds_aug, dim=1)
 
-                y_all = torch.zeros((num_outputs,) + outputs[0].shape).cuda()
+           
+            _, pseudo_outputs_1 = torch.max(preds_orig_prob, dim=1)
+            _, pseudo_outputs_2 = torch.max(preds_aug_prob, dim=1)
+            mtx_bool_conflict = (pseudo_outputs_1 != pseudo_outputs_2)  # bool mask [B, W, H, D]
+            mask_phi_u_a = mtx_bool_conflict.float()
 
+          
+            entropy_orig = -torch.sum(preds_orig_prob * torch.log(preds_orig_prob + 1e-10), dim=1)  # [B, W, H, D]
+            weights = torch.exp(-entropy_orig) * mask_phi_u_a
 
-                loss_seg_dice = 0
+    
+            diff = preds_aug_prob - preds_orig_prob  # [B, C, W, H, D]
+            diff_sq = diff.pow(2).sum(dim=1)          # [B, W, H, D]
+            loss_consistency = (weights * diff_sq).sum() / (weights.sum() + 1e-6)
 
-                y1 = outputs[0]
-                y2= outputs[1]
-                y_prob1 = F.softmax(y1, dim=1)
-                y_prob2 = F.softmax(y2, dim=1)
-                out5, out4, out3, out2 = stage_out1[0], stage_out1[1], stage_out1[2], stage_out1[3]
+            loss_seg_dice = dice_loss(preds_orig_prob[:labeled_bs, 1, ...], label_batch[:labeled_bs, ...] == 1)
+            consistency_weight = get_current_consistency_weight(iter_num // 150)
+            loss = args.lamda * loss_seg_dice + consistency_weight * loss_consistency
 
-                out2_soft = F.softmax(out2, dim=1)
-                out3_soft = F.softmax(out3, dim=1)
-                out4_soft = F.softmax(out4, dim=1)
-                out5_soft = F.softmax(out5, dim=1)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
+            iter_num += 1
 
-                loss_sup1 = losses.dice_loss(y_prob1[:labeled_bs, 1, ...], label_batch[:labeled_bs, ...] == 1)
-                loss_sup2 = losses.dice_loss(y_prob2[:labeled_bs, 1, ...], label_batch[:labeled_bs, ...] == 1)
+            logging.info('Iteration %d: Total loss=%.4f, Dice loss=%.4f, Consistency loss=%.4f' %
+                         (iter_num, loss.item(), loss_seg_dice.item(), loss_consistency.item()))
 
-                loss_sup = loss_sup1 + loss_sup2
+            writer.add_scalar('Loss/total', loss.item(), iter_num)
+            writer.add_scalar('Loss/dice', loss_seg_dice.item(), iter_num)
+            writer.add_scalar('Loss/consistency', loss_consistency.item(), iter_num)
 
-                los2 = losses.dice_loss(out2_soft[:labeled_bs, 1, ...], label_batch[:labeled_bs, ...] == 1)
-                los3 = losses.dice_loss(out3_soft[:labeled_bs, 1, ...], label_batch[:labeled_bs, ...] == 1)
-                los4 = losses.dice_loss(out4_soft[:labeled_bs, 1, ...], label_batch[:labeled_bs, ...] == 1)
-                los5 = losses.dice_loss(out5_soft[:labeled_bs, 1, ...], label_batch[:labeled_bs, ...] == 1)
-                los = 0.6 * los2 + 0.4 * los3 + 0.2 * los4 + 0.1 * los5
-
-
-                y_all[0] = y_prob1
-                y_all[1] = y_prob2
-                loss_seg_dice=loss_sup
-
-                loss_consist = 0
-                loss_conflict=0
-                pixel_consist = 0
-                _, pseudo_outputs_1 = torch.max(y_all[0], dim=1)
-                _, pseudo_outputs_2 = torch.max(y_all[1],dim=1)
-                mtx_bool_conflict = pseudo_outputs_1 != pseudo_outputs_2
-                #mtx_bool_conflict = pseudo_outputs_1 != pseudo_outputs_2
-                conflict_ratio = mtx_bool_conflict.float().sum() / (args.max_samples-args.labelnum)
-
-                # entropy
-                entropy_1 = -torch.sum(y_all[0]* torch.log2(y_all[0] + 1e-10), dim=1)
-                entropy_2 = -torch.sum(y_all[1] * torch.log2(y_all[1]+ 1e-10), dim=1)
-
-                # weighted sum
-                weights_1 = torch.exp(-entropy_1) / (torch.exp(-entropy_1) + torch.exp(-entropy_2))
-                weights_2 = 1 - weights_1
-                weighted_outputs = weights_1.unsqueeze(1) * y_all[0] + weights_2.unsqueeze(
-                    1) * y_all[1]
-
-                weighted_outputs = torch.pow(weighted_outputs, 1.0 / 1)
-                weighted_outputs = weighted_outputs / torch.sum(weighted_outputs, dim=1, keepdim=True)
-
-                # get final outputs
-                pseudo_logits, pseudo_outputs = torch.max(weighted_outputs, dim=1)
-
-                for i in range(num_outputs):
-                    for j in range(num_outputs):
-                        if i != j:
-                            uncertainty_o1 = -1.0 * torch.sum(y_all[i] * torch.log(y_all[i] + 1e-6), dim=1)
-                            uncertainty_o2 = -1.0 * torch.sum(y_all[j] * torch.log(y_all[j] + 1e-6), dim=1)
-                            mask = (uncertainty_o1 > uncertainty_o2).float()
-                            mask1=(uncertainty_o1 <uncertainty_o2).float()
-
-
-                            batch_o, c_o, w_o, h_o, d_o = y_all[j].shape
-                            batch_f, c_f, w_f, h_f, d_f = outfeats[j].shape
-
-                            teacher_o = y_all[j].reshape(batch_o, c_o, -1)
-                            teacher_f = outfeats[j].reshape(batch_f, c_f, -1)
-                            stu_f = outfeats[i].reshape(batch_f, c_f, -1)
-
-                            index = torch.argmax(y_all[j], dim=1, keepdim=True)
-                            prototype_bank = torch.zeros(batch_f, num_classes, c_f).cuda()
-                            for ba in range(batch_f):
-                                for n_class in range(num_classes):
-                                    mask_temp = (index[ba] == n_class).float()
-                                    top_fea = outfeats[j][ba] * mask_temp #伪标签下的特征
-                                    prototype_bank[ba, n_class] = top_fea.sum(-1).sum(-1).sum(-1) / (mask_temp.sum() + 1e-6)#
-
-                            prototype_bank = F.normalize(prototype_bank, dim=-1)
-                            mask_t = torch.zeros_like(y_all[i]).cuda()
-                            for ba in range(batch_o):
-                                for n_class in range(num_classes):
-                                    class_prototype = prototype_bank[ba, n_class]
-                                    mask_t[ba, n_class] = F.cosine_similarity(teacher_f[ba], #来自倒数第二层的特征
-                                                                              class_prototype.unsqueeze(1),#类原型
-
-
-                            weight_pixel_t = (1 - nn.MSELoss(reduction='none')(mask_t, y_all[j])).mean(1) #得到可靠性，越低，距离越大，说明可靠性就越低，这个值越大，越可靠
-                            weight_pixel_t = weight_pixel_t * mask #
-                            weight_pixel_t1=nn.MSELoss(reduction='none')(mask_t, y_all[j]).mean(1)#越大，伪标签越不可靠
-                            weight_pixel_t1=weight_pixel_t1*mask1
-
-                            loss_t = consistency_criterion(y_all[i], torch.argmax(y_all[j], dim=1).detach())
-                            loss_consist += (loss_t * weight_pixel_t.detach()).sum() / (mask.sum() + 1e-6)
-                            loss_conflict+=(loss_t * weight_pixel_t1.detach()).sum() / (mask.sum() + 1e-6)
-
-
-
-                consistency_weight = get_current_consistency_weight(iter_num // 150)
-
-                #loss = args.lamda * loss_seg_dice + consistency_weight * (loss_consist) +args.lamda*los+consistency_weight*loss_conflict
-                optimizer.zero_grad()
-                loss_l=args.lamda * loss_seg_dice+args.lamda * los
-                loss_seg_dice.backward(retain_graph=True)
-
-                # Grad-ReLU策略：在分类层更新参数时，将无监督损失的梯度设置为零
-                for param in model.conv8.parameters():
-                    if param.grad is not None:
-                        param.grad = param.grad * torch.gt(param.data, 0).float()
-                for param in model.conv8_2.parameters():
-                    if param.grad is not None:
-                        param.grad = param.grad * torch.gt(param.data, 0).float()
-
-                optimizer.zero_grad()
-                loss = args.lamda * loss_seg_dice + consistency_weight * (loss_consist) +args.lamda*los+consistency_weight*loss_conflict
-
-                loss.backward()
-                optimizer.step()
-            iter_num = iter_num + 1
-            logging.info('iteration %d : loss : %03f, loss_d: %03f, loss_cosist: %03f,loss_ds:%03f,loss_ub:%03f' %
-                         (iter_num, loss, loss_seg_dice, loss_consist,los,loss_conflict))
-
-            writer.add_scalar('Labeled_loss/loss_seg_dice', loss_seg_dice, iter_num)
-            # writer.add_scalar('Labeled_loss/loss_seg_ce', loss_seg, iter_num)
-            writer.add_scalar('Co_loss/consistency_loss', loss_consist, iter_num)
-
+            # --- 验证与保存模型 ---
             if iter_num >= 800 and iter_num % 200 == 0:
                 model.eval()
                 if args.dataset_name == "LA":
@@ -305,7 +207,6 @@ if __name__ == "__main__":
                                                           stride_xy=18,
                                                           stride_z=4,
                                                           dataset_name='LA')
-
                 elif args.dataset_name == "Pancreas_CT":
                     dice_sample = test_patch.var_all_case(model,
                                                           num_classes=num_classes,
@@ -313,24 +214,25 @@ if __name__ == "__main__":
                                                           stride_xy=16,
                                                           stride_z=16,
                                                           dataset_name='Pancreas_CT')
+
                 if dice_sample > best_dice:
                     best_dice = dice_sample
                     save_mode_path = os.path.join(snapshot_path, 'iter_{}_dice_{}.pth'.format(iter_num, best_dice))
                     save_best_path = os.path.join(snapshot_path, '{}_best_model.pth'.format(args.model))
                     torch.save(model.state_dict(), save_mode_path)
                     torch.save(model.state_dict(), save_best_path)
-                    logging.info("save best model to {}".format(save_mode_path))
-                writer.add_scalar('Var_dice/Dice', dice_sample, iter_num)
-                writer.add_scalar('Var_dice/Best_dice', best_dice, iter_num)
+                    logging.info("Saved best model to {}".format(save_mode_path))
+
+                writer.add_scalar('Validation/Dice', dice_sample, iter_num)
+                writer.add_scalar('Validation/Best_Dice', best_dice, iter_num)
                 model.train()
 
             if iter_num >= max_iterations:
-                save_mode_path = os.path.join(snapshot_path, 'iter_' + str(iter_num) + '.pth')
+                save_mode_path = os.path.join(snapshot_path, 'iter_{}.pth'.format(iter_num))
                 torch.save(model.state_dict(), save_mode_path)
-                logging.info("save model to {}".format(save_mode_path))
+                logging.info("Saved final model to {}".format(save_mode_path))
                 break
         if iter_num >= max_iterations:
-            net = net_factory(net_type=args.model, in_chns=1, class_num=num_classes, mode="train")
             iterator.close()
             break
 
